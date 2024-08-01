19182f3885ce990d93d4b2e3196f97bdc2b8f2d0
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-08-01 15:08:34 +0200

    Displayed my sum

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
 function startGame() {
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackjack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
-    console.log(message)
+    messageEl.textContent = message
 }
